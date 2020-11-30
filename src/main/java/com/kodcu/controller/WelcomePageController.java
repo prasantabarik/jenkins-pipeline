8663d6acbf21660ab995095c92d0484a987272d8
prasantabarik <54621979+prasantabarik@users.noreply.github.com>
2020-11-30 23:05:27 +0100

    Update WelcomePageController.java

--- src/main/java/com/kodcu/controller/WelcomePageController.java
+++ src/main/java/com/kodcu/controller/WelcomePageController.java
@@ -13,6 +13,12 @@ public class WelcomePageController {
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String getPage(){
+        final String uri = "http://localhost:3500/v1.0/invoke/multiplyapp/method/multiply";
+
+    RestTemplate restTemplate = new RestTemplate();
+    String result = restTemplate.getForObject(uri, String.class);
+
+    System.out.println(result);
         return "index";
     }
 }

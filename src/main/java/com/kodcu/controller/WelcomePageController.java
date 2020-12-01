a50295899de1f4dac12f2796229865e1d010e758
prasantabarik <54621979+prasantabarik@users.noreply.github.com>
2020-12-01 10:59:49 +0100

    Update WelcomePageController.java

--- src/main/java/com/kodcu/controller/WelcomePageController.java
+++ src/main/java/com/kodcu/controller/WelcomePageController.java
@@ -8,6 +8,7 @@ package
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.client.RestTemplate;
 
 @Controller
 public class WelcomePageController {
